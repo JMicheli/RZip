2edf8826708754e30c4c551b8069cb23ce4c4813
Joseph Micheli <josephwmicheli@gmail.com>
2024-07-06 16:16:13 -0500

    Update packages and clean up FileTree component.

--- apps/gui/src/FileTree.tsx
+++ apps/gui/src/FileTree.tsx
@@ -1,34 +1,36 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Tree, TreeDataNode } from "antd";
-import { CarryOutOutlined, CheckOutlined, FormOutlined } from "@ant-design/icons";
+import { CarryOutOutlined, CheckOutlined, FormOutlined, FileOutlined } from "@ant-design/icons";
 
 const iconMapper: Record<string, JSX.Element> = {
   CarryOutOutlined: <CarryOutOutlined />,
   CheckOutlined: <CheckOutlined />,
-  FormOutlined: <FormOutlined />
+  FormOutlined: <FormOutlined />,
+  FileOutlined: <FileOutlined />
 };
 
 const getIcon = (iconName?: string) => {
   return iconName ? iconMapper[iconName] : undefined;
 };
+
+const mapIconsRecursively = (node: TreeDataNode): TreeDataNode => {
+  return {
+    ...node,
+    icon: getIcon(node.icon as string),
+    children: node.children ? node.children.map(mapIconsRecursively) : undefined
+  };
+};
+
 function FileTree() {
   const [treeData, setTreeData] = useState([] as TreeDataNode[]);
 
   useEffect(() => {
     invoke("get_file_tree")
       .then((data) => {
-        let typed_data = data as any[];
-        const mappedData = typed_data.map((node) => ({
-          ...node,
-          icon: getIcon(node.icon),
-          children: node.children
-            ? node.children.map((child: { icon: string | undefined }) => ({
-                ...child,
-                icon: getIcon(child.icon)
-              }))
-            : undefined
-        }));
+        const typed_data = data as TreeDataNode[];
+        const mappedData = typed_data.map(mapIconsRecursively);
+
         setTreeData(mappedData);
       })
       .catch((error) => console.error("Error fetching file tree:", error));
@@ -37,7 +39,7 @@ function FileTree() {
   return (
     <div>
       <Tree
-        showLine={{ showLeafIcon: true }}
+        showLine={{ showLeafIcon: false }}
         showIcon
         defaultExpandedKeys={["0-0-0"]}
         onSelect={(selectedKeys, info) => console.log("selected", selectedKeys, info)}
